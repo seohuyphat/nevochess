6bf2b31fea8b00e7c66ee2d7ed6a08a6c00621ee
sakur.deagod <sakur.deagod@d76e4d38-d5d3-11de-82c3-17aed804ed57>
2010-01-17 16:06:12 +0000

    NevoChess V1.0 AppStore version

---
+++ tags/v1.0AppStore/Classes/CChessGame.h
@@ -0,0 +1,93 @@
+/***************************************************************************
+ *  Copyright 2009 Nevo Hua  <nevo.hua@playxiangqi.com>                    *
+ *                                                                         * 
+ *  This file is part of NevoChess.                                        *
+ *                                                                         *
+ *  NevoChess is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  NevoChess is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with NevoChess.  If not, see <http://www.gnu.org/licenses/>.     *
+ ***************************************************************************/
+
+#import <CoreGraphics/CoreGraphics.h>
+#import <QuartzCore/QuartzCore.h>
+#import "Game.h"
+#import "AIEngine.h"
+#import "Referee.h"
+
+#define INVALID_MOVE         (-1)
+#define TOSQUARE(row, col)   (16 * ((row) + 3) + ((col) + 3))
+#define COLUMN(sq)           ((sq) % 16 - 3)
+#define ROW(sq)              ((sq) / 16 - 3)
+
+
+/* Possible game result */
+enum{
+    kXiangQi_Unknown = -1,
+    kXiangQi_InPlay,
+    kXiangQi_YouWin,
+    kXiangQi_ComputerWin,
+    //we need this state because you might play with other online player
+    kXiangqi_YouLose,
+    kXiangQi_Draw,
+    kXiangQi_OverMoves,
+};
+
+/* Possible AI engines.
+ * TODO: We should use prefix "kNevoChess" to make our constants
+ *       unique from others (to avoid naming conflicts).
+ *       Also, once we are done with the AI framework, then there will be
+ *       no need for these constants.
+ */
+enum {
+    kNevoChess_AI_xqwlight,
+    kNevoChess_AI_haqikid,
+    kNevoChess_AI_xqwlight_objc,
+};
+
+
+@class RectGrid;
+@class Piece;
+
+@interface CChessGame : Game
+{
+    RectGrid *_grid;
+    
+    NSMutableArray *_pieceBox;
+    
+    int _aiType;
+    Referee  *_referee;
+    AIEngine *_aiEngine;
+
+    int game_result;
+}
+
+- (void)setupCChessPieces;
+- (void)x_createPiece: (NSString*)imageName row: (int)row col: (int)col forPlayer: (unsigned)playerNo;
+- (void)x_movePiece:(Piece*)piece toRow:(int)row toCol:(int)col;
+- (Piece*)x_getPieceAtRow:(int)row col:(int)col;
+
+- (int)  getRobotMove:(int*)captured;
+- (int) humanMove:(int)row1 fromCol:(int)col1
+             toRow:(int)row2   toCol:(int)col2;
+
+- (void)setSearchDepth:(int)depth;
+- (int) generateMoveFrom:(int)sqSrc moves:(int*)mvs;
+- (BOOL)isLegalMove:(int)mv;
+- (int)checkGameStatus:(BOOL)isAI;
+- (int) get_sdPlayer;
+- (void)resetCChessPieces;
+- (void)reset_game;
+
+@property (nonatomic,readonly) RectGrid *_grid;
+@property (nonatomic,readonly) int game_result;
+
+@end
---
+++ tags/v1.0AppStore/Classes/OptionsViewController.m
@@ -0,0 +1,219 @@
+/***************************************************************************
+ *  Copyright 2009 Nevo Hua  <nevo.hua@playxiangqi.com>                    *
+ *                                                                         * 
+ *  This file is part of NevoChess.                                        *
+ *                                                                         *
+ *  NevoChess is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  NevoChess is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with NevoChess.  If not, see <http://www.gnu.org/licenses/>.     *
+ ***************************************************************************/
+
+#import "OptionsViewController.h"
+#import "NevoChessAppDelegate.h"
+#import "QuartzUtils.h"
+#import "GenericSettingViewController.h"
+#import "AISelectionViewController.h"
+
+@implementation OptionsViewController
+
+/*
+- (id)initWithStyle:(UITableViewStyle)style {
+    // Override initWithStyle: if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+    if (self = [super initWithStyle:style]) {
+    }
+    return self;
+}
+*/
+
+
+- (void)viewDidLoad 
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithCGColor:GetCGPatternNamed(@"board_320x480.png")];
+    self.title = NSLocalizedString(@"Settings", @"");
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+
+
+- (void)viewWillAppear:(BOOL)animated 
+{
+    [super viewWillAppear:animated];
+    NSArray *cells = [(UITableView*)self.view visibleCells];
+    if([cells count] > 0) {
+        UITableViewCell *cell = [cells objectAtIndex:1];
+        cell.detailTextLabel.text = [[NSUserDefaults standardUserDefaults] objectForKey:@"AI"];
+    }
+    [((NevoChessAppDelegate*)[[UIApplication sharedApplication] delegate]).navigationController setNavigationBarHidden:NO animated:NO];
+}
+
+/*
+- (void)viewDidAppear:(BOOL)animated 
+{
+    [super viewDidAppear:animated];
+    
+}
+*/ 
+
+- (void)viewWillDisappear:(BOOL)animated 
+{
+	[super viewWillDisappear:animated];
+    [((NevoChessAppDelegate*)[[UIApplication sharedApplication] delegate]).navigationController setNavigationBarHidden:YES animated:NO];
+}
+
+/*
+- (void)viewDidDisappear:(BOOL)animated {
+	[super viewDidDisappear:animated];
+}
+*/
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning 
+{
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload 
+{
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
+}
+
+
+#pragma mark Table view methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView 
+{
+    return 1;
+}
+
+
+// Customize the number of rows in the table view.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section 
+{
+    return 2;
+}
+
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath 
+{
+    UITableViewCell *cell = nil;
+    // section 0
+    if (indexPath.row == 0) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"generic_setting"];
+        if(!cell) {
+            cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"generic_setting"] autorelease];
+        }
+        cell.textLabel.font = [UIFont systemFontOfSize:20.0f];
+        cell.textLabel.text = NSLocalizedString(@"General", @"");
+        cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+        return cell;
+    }
+    
+    if (indexPath.row == 1) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"ai_setting"];
+        if(!cell) {
+            cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"ai_setting"] autorelease];
+        }
+        cell.textLabel.font = [UIFont systemFontOfSize:20.0f];
+        cell.textLabel.text = NSLocalizedString(@"AI_Setting_Key", @"");
+        cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+        cell.detailTextLabel.text = [[NSUserDefaults standardUserDefaults] objectForKey:@"AI"];
+        return cell;
+    }
+    return cell;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath 
+{
+    // Navigation logic may go here. Create and push another view controller.
+	// AnotherViewController *anotherViewController = [[AnotherViewController alloc] initWithNibName:@"AnotherView" bundle:nil];
+	// [self.navigationController pushViewController:anotherViewController];
+	// [anotherViewController release];
+    if (indexPath.row == 0) {
+        //generic settings
+        SettingViewController *genericController = [[SettingViewController alloc] initWithNibName:@"GenericSettingViewController" bundle:nil];
+        [((NevoChessAppDelegate*)[[UIApplication sharedApplication] delegate]).navigationController pushViewController:genericController animated:YES];
+        [genericController release];  
+    }
+    
+    if (indexPath.row == 1) {
+        //AI selection
+        AISelectionViewController *aiController = [[AISelectionViewController alloc] initWithNibName:@"AISelectionView" bundle:nil];
+        [((NevoChessAppDelegate*)[[UIApplication sharedApplication] delegate]).navigationController pushViewController:aiController animated:YES];
+        [aiController release]; 
+        
+    }
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+
+- (void)dealloc 
+{
+    [super dealloc];
+}
+
+
+@end
+
